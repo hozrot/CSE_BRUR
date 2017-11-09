d29e869d5f7e9caa3332f6b73709e1dc5ad84a6e
hozrot <hozrot.ali64@gmail.com>
2017-11-09 10:08:57 +0600

    morning test

--- social_work/models.py
+++ social_work/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Student(models.Model):
 	name 			= 	models.CharField(max_length=300)
 	reg_no          =   models.CharField(max_length=50)
