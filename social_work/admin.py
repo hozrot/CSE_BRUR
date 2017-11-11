3e70946e1b52b153bad53f2169fd6d0b4aab036c
hozrot <hozrot.ali64@gmail.com>
2017-11-12 02:01:26 +0600

    merit , student and teacher table added

--- social_work/admin.py
+++ social_work/admin.py
@@ -1,6 +1,1 @@
 from django.contrib import admin
-from .models import Student
-class Studentlist(admin.ModelAdmin):
-	list_display = ['name','contact_number','photo']
-
-admin.site.register(Student,Studentlist)
